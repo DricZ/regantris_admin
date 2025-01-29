018d80bc83e75010b0dfab3f19ea476907dde7b0
Audrico <drcsm2013@gmail.com>
2025-01-29 23:20:41 +0700

    anu

--- app/Models/Hotels.php
+++ app/Models/Hotels.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class hotels extends Model
+class Hotels extends Model
 {
     use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
